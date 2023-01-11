4ba234f44ffaae7ef66b4d0a5118a81841c5fe9d
ositv <122423163+ositv@users.noreply.github.com>
2023-01-11 10:27:56 +0530

    Add files via upload

---
+++ sony.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./content/css/style.css">
+    <link rel="stylesheet" href="./content/css/searchbox.css">
+    <link rel="stylesheet" href="./content/css/channels.css">
+    <link rel="stylesheet" href="./content/css/footer.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">      
+    <title><!------TITEL-WEBSITE--------></title>
+</head>
+<body>
+
+    <!--==================================NAVBAR==============================-->
+    
+<div class="header">
+    <div class="brand"><span><h3>Techmax</h3>&nbsp;<h6>LIVE</h6></span>
+        <a id=""menu" href="#"> <img id="menuimg" src="./content/img/app_logo/menu.png"> </a> </div>
+   
+   <div class="links" id="drop">
+        <a href="./index.php"  class="btn text-light" >JIO TV</a>
+        <a href="./zee5.php" class="btn text-light">Zee5</a>
+        <a href="./sony.php" class="btn bg-danger text-light">Sony Liv</a>
+       <!--<a href="./contact.html" class="btn text-light">Contact</a>-->
+    </div>
+</div>
+
+<!--=================================Search==============================-->
+
+<div class="container" class="search">
+<input id="search" type="text" placeholder="Search Your Channel">
+</div>
+
+
+
+
+ <!--==================================CHANNELS==============================-->
+<div class="container-fluid" id="channels">
+<div class="row">
+
+    <!--------------------Paste New Channel Down Here-------------------------->
+
+
+
+
+    <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_SAB"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_SAB.png" style="height: 120px">
+        <div class="info">
+        <span>Sony SAB</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_HD.png" style="height: 120px" src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_HD.png">
+        <div class="info">
+        <span>Sony HD</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_SAB_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_SAB_HD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony SAB HD</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Pal"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Pal.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Pal</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Pix_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Pix_HD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Pix HD</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Max_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/SET_MAX.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Max SD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Max_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Max_HD.png" style="height: 120px" >
+        <div class="info">
+        <span>Sony Max HD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_MAX2"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_MAX2.png" style="height: 120px">
+        <div class="info">
+        <span>Sony MAX2</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Six_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Six_HD.png" style="height: 120px" ">
+        <div class="info">
+        <span>Sony Six HD</span>
+        </div>
+        </a>
+        </div></div><div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Wah"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Wah.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Wah</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_SD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony SD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Pix_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Pix_SD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Pix SD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten_HD.png" style="height: 120px" >
+        <div class="info">
+        <span>Ten 1 HD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten2_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten2_HD.png" style="height: 120px">
+        <div class="info">
+        <span>Ten 2 HD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten3_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten3_HD.png" style="height: 120px">
+        <div class="info">
+        <span>Ten 3 HD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten_1"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten_1.png" style="height: 120px">
+        <div class="info">
+        <span>Ten 1</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten_2"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten_2.png" style="height: 120px">
+        <div class="info">
+        <span>Ten 2</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Ten_3"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Ten_3.png" style="height: 120px">
+        <div class="info">
+        <span>Ten 3</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_BBC_Earth_HD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_HD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony BBC Earth HD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Six_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Six_SD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Six SD</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Yay_Hindi"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Yay_Hindi.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Yay Hindi</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_BBC_Earth_HD_Telugu"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_HD_Telugu.png" style="height: 120px">
+        <div class="info">
+        <span>Sony BBC Earth HD Telugu</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_BBC_Earth_HD_Tamil"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_HD_Tamil.png" style="height: 120px">
+        <div class="info">
+        <span>Sony BBC Earth HD Tamil</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Marathi_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Marathi_SD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Marathi SD</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_aath"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_aath.png" style="height: 120px">
+        <div class="info">
+        <span>Sony aath</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_BBC_Earth_SD"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_SD.png" style="height: 120px">
+        <div class="info">
+        <span>Sony BBC Earth SD</span>
+        </div>
+        </a>
+        </div></div>
+        
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_BBC_Earth_HD_English"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_HD_English.png" style="height: 120px">
+        <div class="info">
+        <span>Sony BBC Earth HD English</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Yay_Telugu"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Yay_Telugu.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Yay Telugu</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Yay_Telugu"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Yay_Telugu.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Yay Telugu</span>
+        </div>
+        </a>
+        </div></div>
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2"> 
+        <a href="playsony.php?c=Sony_Yay_Tamil"  > <div class="card">
+        <img  src="http://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Yay_Tamil.png" style="height: 120px">
+        <div class="info">
+        <span>Sony Yay Tamil</span>
+        </div>
+        </a>
+        </div></div>
+        </div>
+        
+        
+
+
+</div>
+</div>
+
+
+   <!-----------------------------FOOTER-------------------------->
+
+   <div class="footer bg-dark text-light">
+Tamplate Designed By&nbsp;<a href="https://t.me/techmaxpro">Techmaxpro</a>
+   </div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+             
+<script>
+$(document).ready(function(){
+  $("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".col-sm-4").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+
+
+
+        $(document).ready(function(){
+            $("#menuimg").click(function(){
+               $("#drop").slideToggle("fast"); 
+            });
+        });
+</script>
+</body></html>
